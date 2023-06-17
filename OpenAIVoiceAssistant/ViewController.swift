a3dee629ccf1e92fd2dc1acc4e59b6aab8c993c1
Murat Kucuk <muratkucuk099@gmail.com>
2023-06-17 15:37:14 +0300

    Initial Commit

---
+++ OpenAIVoiceAssistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenAIVoiceAssistant
+//
+//  Created by Mac on 17.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
